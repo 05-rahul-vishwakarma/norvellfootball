71bc9c85fe3457733a0452fac0c18d03fb687d6d
05-rahul-vishwakarma <rahul930vishwakarma@gmail.com>
2024-02-18 14:55:35 +0530

    few changes

--- novellfootball/src/app/page.js
+++ novellfootball/src/app/page.js
@@ -1,14 +1,15 @@
 import Image from "next/image";
+import { FaCirclePlus } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <main className="h-screen ">
       <div className=" h-[10%]   ">
-        <div className=" flex justify-between ">
-          <div className="flex ml-[.5rem] border-2 border-black h-max w-[30%] ">
-            ₹<p>100000</p>
-            <span className="w-[1rem] " >
-
+        <div className=" flex justify-between mt-[.5rem] ">
+          <div className="flex ml-[.5rem] border-2 border-black h-max w-[30%] place-items-center justify-between py-[.1rem] px-[.2rem] ">
+            <span className="flex " >₹<p>100000</p></span>
+            <span className="w-[1rem] ">
+              <FaCirclePlus />
             </span>
           </div>
 
