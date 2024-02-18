b3f0a9470126e9608aa77e0a1569bfffa0977112
05-rahul-vishwakarma <rahul930vishwakarma@gmail.com>
2024-02-18 14:02:02 +0530

    first commit

---
+++ novellfootball/src/app/matches/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>match page</div>
+  )
+}
+
+export default page
---
+++ novellfootball/src/app/page.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-screen ">
+      <div className=" h-[10%]   ">
+        <div className=" flex justify-between ">
+          <div className="flex ml-[.5rem] border-2 border-black h-max w-[30%] ">
+            ₹<p>100000</p>
+            <span className="w-[1rem] " >
+
+            </span>
+          </div>
+
+          <div className="flex mr-[.5rem] place-items-center ">
+            <span className="leading-5 mt-[.5rem] mr-[.3rem] ">
+              <h1 className="font-semibold">WELCOME BACK</h1>
+              <h3 className="">DARGON</h3>
+            </span>
+            <span className=" border-2 border-black h-[4rem] w-[4rem] rounded-[100%]  "></span>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
