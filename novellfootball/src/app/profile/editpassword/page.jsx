afe67b1bfd273bec44215dcde9ca39ddd9755142
I-Vishal-kumar <vishal2983kumar@gmail.com>
2024-02-24 11:06:33 +0530

    created help page and change password page in profile

---
+++ novellfootball/src/app/profile/editpassword/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import VerificationPopup from "@/app/components/VerificationPopup";
+import React from "react";
+
+const Page = () => {
+  return (
+    <section className="h-full w-full">
+      <VerificationPopup />
+    </section>
+  );
+};
+
+export default Page;
