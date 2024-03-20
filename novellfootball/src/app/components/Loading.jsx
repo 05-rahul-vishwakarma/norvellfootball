3ee3082a80287f3a4daf9e7e44b7fddebc8a322e
05-rahul-vishwakarma <rahul930vishwakarma@gmail.com>
2024-03-20 16:15:08 +0530

    update

--- novellfootball/src/app/components/Loading.jsx
+++ novellfootball/src/app/components/Loading.jsx
@@ -4,12 +4,13 @@ import Image from "next/image";
 function Loading() {
   return (
     <div className="w-dvw h-dvh absolute top-0 left-0  z-[20] ">
-      <div className="w-full h-screen flex flex-col justify-center items-center   ">
-        <div className=" h-[25%] relative " >
-          <div className="football ">
-            <Image src={"/load.png"} alt="loading" width={50} height={50} />
+      <div className="suport  h-full grid place-items-center ">
+        <div className="smash">
+          <div className="shadow">
+            <div className="rotateimg">
+               <Image src={'/load.png'} alt="loading" width={50} height={50} className="w-full h-full " />
+            </div>
           </div>
-          <div className="shadow"></div>
         </div>
       </div>
     </div>
