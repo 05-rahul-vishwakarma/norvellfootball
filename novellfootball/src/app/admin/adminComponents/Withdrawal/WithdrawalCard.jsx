37c6dc580e5e5b5414adfeda72723e5bdddb238d
I-Vishal-kumar <vishal2983kumar@gmail.com>
2024-03-27 09:27:01 +0530

    check

--- novellfootball/src/app/admin/adminComponents/Withdrawal/WithdrawalCard.jsx
+++ novellfootball/src/app/admin/adminComponents/Withdrawal/WithdrawalCard.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useFormState } from "react-dom";
 import { updateTransaction } from "./Action";
 import { Button, Listeners } from "../Bets/BetCard";
