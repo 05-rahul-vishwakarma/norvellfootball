8ae90d24e60fd5afd0ced76390f852e2b02fe18b
I-Vishal-kumar <vishal2983kumar@gmail.com>
2024-05-28 17:24:54 +0530

    updated email

--- novellfootball/src/app/helpers/otp.js
+++ novellfootball/src/app/helpers/otp.js
@@ -1,51 +1,51 @@
 import nodemailer from "nodemailer";
 
 export async function sendPhoneOtp(number, otp) {
-  try {
-    let baseUrl = "https://www.fast2sms.com/dev/bulkV2";
-    const querParams = new URLSearchParams({
-      authorization:
-        "w0d8sQkyt4aIiJ5BcKFfVPxLueZSXoMqATgmUlW1G97Y6NvjzRhBczyNPs9SXtpU6jMurlLqGwavWZJ5",
-      variables_values: `${otp}`,
-      route: "otp",
-      numbers: number,
-    });
-    const headers = {
-      "cache-control": "no-cache",
-    };
-    const url = `${baseUrl}?${querParams.toString()}`;
+    try {
+        let baseUrl = "https://www.fast2sms.com/dev/bulkV2";
+        const querParams = new URLSearchParams({
+            authorization:
+                "w0d8sQkyt4aIiJ5BcKFfVPxLueZSXoMqATgmUlW1G97Y6NvjzRhBczyNPs9SXtpU6jMurlLqGwavWZJ5",
+            variables_values: `${otp}`,
+            route: "otp",
+            numbers: number,
+        });
+        const headers = {
+            "cache-control": "no-cache",
+        };
+        const url = `${baseUrl}?${querParams.toString()}`;
 
-    let res = await fetch(url, { method: "GET", headers });
-    res = await res.json();
+        let res = await fetch(url, { method: "GET", headers });
+        res = await res.json();
 
-    if (!res) throw Error("Server error");
-    if (res?.return === true) return true;
-    return false;
-  } catch (error) {
-    return false;
-  }
+        if (!res) throw Error("Server error");
+        if (res?.return === true) return true;
+        return false;
+    } catch (error) {
+        return false;
+    }
 }
 
 export async function sendEmailOtp(EmailId, otp) {
-  try {
-    let transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "norvellfootball0@gmail.com",
-        pass: "gpjukemzttiykwqd",
-      },
-    });
+    try {
+        let transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: "norvellfootball0@gmail.com",
+                pass: "gpjukemzttiykwqd",
+            },
+        });
 
-    let mailOptions = {
-      from: "norvellfootball0@gmail.com",
-      to: `${EmailId}`,
-      subject: "Norvell football",
-      text: `Your OTP for varification is ${otp}`,
-    };
+        let mailOptions = {
+            from: "norvellfootball0@gmail.com",
+            to: `${EmailId}`,
+            subject: "Norvell football",
+            text: `Your OTP for varification is ${otp}`,
+        };
 
-    await transporter.sendMail(mailOptions);
-    return true;
-  } catch (error) {
-    return false;
-  }
+        await transporter.sendMail(mailOptions);
+        return true;
+    } catch (error) {
+        return false;
+    }
 }
